f6bc1bab5778fc0165ffe8bb0bcc07fc6ae41a00
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-05-09 09:26:20 +0700

    add view to screen

--- hooks/action-button-gb_attendances.php
+++ hooks/action-button-gb_attendances.php
@@ -1,7 +1,7 @@
 <?php 
 
 return '
-<a href="'.routeTo('guestbook/set-to-screen',['id' => $data->id]).'" class="btn btn-sm btn-secondary" target="_blank"><i class="mdi mdi-crop-free"></i> Tampilkan ke Screen</a> 
+<a href="'.routeTo('guestbook/set-to-screen',['id' => $data->id]).'" class="btn btn-sm btn-secondary"><i class="mdi mdi-crop-free"></i> Tampilkan ke Screen</a> 
 ';
 
 ?>
--- process/set-to-screen.php
+++ process/set-to-screen.php
@@ -6,7 +6,7 @@ $id = $_GET['id'];
 $db = new Database;
 $db->query = "SELECT gb_attendances.* FROM gb_attendances JOIN gb_guests ON gb_guests.id = gb_attendances.guest_id WHERE gb_attendances.id = $id";
 $attendance = $db->exec('single');
-$db->update('gb_attendances', ['status' => 'ON QUEUE'], ['id'=>$id]);
+$db->update('gb_attendances', ['screen_status' => 'ON QUEUE'], ['id'=>$id]);
 set_flash_msg(['success' => "Sukses menampilkan ke screen"]);
 
 header('location:'.routeTo('crud/index', ['table'=>'gb_attendances','filter' => ['event_id' => $attendance->event_id]]));

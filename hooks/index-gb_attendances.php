3c47a8ea497eb023a33684160a288c33458cac5b
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-05-09 12:43:01 +0700

    update filter gb attendances

--- hooks/index-gb_attendances.php
+++ hooks/index-gb_attendances.php
@@ -1,15 +1,37 @@
 <?php
 
-if ($filter) {
+$having = "";
+$where = "";
+
+if(!empty($search))
+{
+    $_where = [];
+    foreach([
+        'gb_guests.name',
+        'users.name',
+        'gb_events.name'
+    ] as $col)
+    {
+        $_where[] = "$col LIKE '%$search%'";
+    }
+
+    $where = "WHERE (".implode(' OR ',$_where).")";
+}
+
+if($filter)
+{
     $filter_query = [];
-    foreach ($filter as $f_key => $f_value) {
+    foreach($filter as $f_key => $f_value)
+    {
         $filter_query[] = "$f_key = '$f_value'";
     }
 
     $filter_query = implode(' AND ', $filter_query);
 
-    $where .= (empty($where) ? 'WHERE ' : ' AND ') . $filter_query;
+    $having = (empty($having) ? 'HAVING ' : ' AND ') . $filter_query;
 }
+
+$where = $where ." ". $having;
 
 $query = "SELECT gb_attendances.id AS id,
                      gb_guests.name AS guest_name,
@@ -23,15 +45,21 @@ $query = "SELECT gb_attendances.id AS id,
                 JOIN gb_events ON gb_guests.event_id = gb_events.id
                 $where ORDER BY " . $col_order . " " . $order[0]['dir'] . " LIMIT $start,$length";
 
+$db->query = $query;
+$data = $db->exec('all');
 
-$db->query = "SELECT COUN(*) FROM gb_attendances
+$db->query = "SELECT gb_attendances.id AS id,
+    gb_guests.name AS guest_name,
+    gb_events.id AS event_id,
+    gb_attendances.created_at,
+    users.name AS user_name,
+    gb_events.name AS event_name 
+FROM gb_attendances
 JOIN users ON gb_attendances.created_by = users.id
 JOIN gb_guests ON gb_attendances.guest_id = gb_guests.id
 JOIN gb_events ON gb_guests.event_id = gb_events.id
 $where ORDER BY " . $col_order . " " . $order[0]['dir'] . " LIMIT $start,$length";
 
-$db->query = $query;
-$data = $db->exec('all');
 $total = $db->exec('exists');
 
 return compact('data', 'total');
